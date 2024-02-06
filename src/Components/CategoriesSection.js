c8c914f903767b6ffb9f3cb29916b5da73dc1342
oindil <oindilgolder303@gmail.com>
2024-02-06 23:13:20 +0530

    need to add categoried dropdown

--- src/Components/CategoriesSection.js
+++ src/Components/CategoriesSection.js
@@ -16,6 +16,53 @@ function CategoriesSection() {
         { image: 'https://www.adaptivewfs.com/wp-content/uploads/2020/07/logo-placeholder-image.png', name: 'Two Wheelers', chevron: true, size: { height: 80, width: 80 } }
     ]
 
+    const Fashion_DD_data = [
+        {name:`Men's Top Wear`,link:'#link'},
+        {name:`Men's Bottom Wear`,link:'#link'},
+        {name:`Women Ethnic`,link:'#link'},
+        {name:`Women Western`,link:'#link'},
+        {name:`Men Footwear`,link:'#link'},
+        {name:`Women Footwear`,link:'#link'},
+        {name:`Watches and Accessories`,link:'#link'},
+        {name:`Bags, Suitcases & Luggage`,link:'#link'},
+        {name:`Kids`,link:'#link'},
+        {name:`Essentials`,link:'#link'},
+        {name:`Winter`,link:'#link'}
+    ]
+    const Electronics_DD_data = [
+        {name:`Audio`,link:'#link'},
+        {name:`Electronics GST Store`,link:'#link'},
+        {name:`Computer Peripherals`,link:'#link'},
+        {name:`Gaming`,link:'#link'},
+        {name:`Health & Personal Care`,link:'#link'},
+        {name:`Laptop Accessories`,link:'#link'},
+        {name:`Laptop and Desktop`,link:'#link'},
+        {name:`MobileAccessory`,link:'#link'},
+        {name:`Powerbank`,link:'#link'}
+    ]
+    
+    const HomeFurniture_DD_data = [
+        {name:`Home Furnishings`,link:'#link'},
+        {name:`Furniture Studio`,link:'#link'},
+        {name:`Living Room Furniture`,link:'#link'},
+        {name:`Kitchen & Dining`,link:'#link'},
+        {name:`Bedroom Furniture`,link:'#link'},
+        {name:`Space Saving Furniture`,link:'#link'}
+    ]
+    const BeautyToys_DD_data = [
+        {name:`Beuty & Personal Care`,link:'#link'},
+        {name:`Men's Grooming`,link:'#link'},
+        {name:`Food & Drinks`,link:'#link'},
+        {name:`Nutrition & Health Care`,link:'#link'},
+        {name:`Baby Care`,link:'#link'}
+    ]
+    const TwoWheelers_DD_data = [
+        {name:`Petrol Vehicles`,link:'#link'},
+        {name:`Electric Vehicles`,link:'#link'}
+    ]
+
+    
+
     const handleMouseOver = (index) => {
         setMouseOver(index)
     }

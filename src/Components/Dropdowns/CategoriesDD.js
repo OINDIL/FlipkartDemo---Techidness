a9812227d3b963b8641cee7bd91fd18afe2b205f
oindil <oindilgolder303@gmail.com>
2024-02-08 14:31:06 +0530

    added hover effect on categories dropdown

--- src/Components/Dropdowns/CategoriesDD.js
+++ src/Components/Dropdowns/CategoriesDD.js
@@ -3,10 +3,11 @@ import React from 'react'
 
 function CategoriesDD(props) {
     const { mouseOver , dropDown} = props
+    // console.log(mouseOver);
     return (
         <>
             {mouseOver ?
-                (<div id="categories-dd-container" className='fashion'>
+                (<div id="categories-dd-container" className={mouseOver === 2 ? `fashion` : mouseOver === 3 ? 'electronics' :mouseOver === 4 ? `home-furniture`: mouseOver  === 7 ? `beauty-toys`: `two-wheelers`}>
                     <ul>
                         {dropDown.map((item,index)=>{
                             return(
